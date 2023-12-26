b490be03d5d0087f6a0bd8e598bf3c9b47763703
Egor213 <112801343+Egor213@users.noreply.github.com>
2023-12-26 18:55:37 +0300

    Update tink_1.cpp

--- tink_1.cpp
+++ tink_1.cpp
@@ -1,25 +1,23 @@
 #include <iostream>
 #include <string>
 #include <vector>
-
-bool isTink(const std::string& s)
+#include <algorithm>
+bool isTink(std::string &s)
 {
-    std::vector<char> temp_vec = {'T','I','N','K','O','F','F'};
-    std::vector<char> temp2_vec(s.begin(), s.end());
-    return (temp_vec == temp2_vec) && s.length() == temp_vec.size();
+    std::string temp = "FFIKNOT";
+    std::sort(s.begin(), s.end());
+    return temp == s;
 }
 
-
-int main() {
+int main()
+{
     int a;
     std::cin >> a;
     std::string line;
-    while(std::cin >> line)
+    while (std::cin >> line)
     {
         auto answer = isTink(line) ? "Yes" : "No";
-        std::cout << answer << std::endl;   
-    } 
-    
-    
+        std::cout << answer << std::endl;
+    }
     return 0;
 }
