130df3c360fba7a9c91c688761fe2cd99b75be2a
Egor213 <zamok7707@gmail.com>
2024-02-07 21:00:09 +0300

    ok

---
+++ Patterns/Abstact_fabric/build/CMakeFiles/Fabr.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Fabr.dir/main.cpp.o"
+  "CMakeFiles/Fabr.dir/main.cpp.o.d"
+  "Fabr"
+  "Fabr.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Fabr.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
