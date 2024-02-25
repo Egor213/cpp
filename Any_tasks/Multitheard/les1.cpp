b0ed751987f731af7b5e7fed8aa2f90429820ce9
Egor213 <zamok7707@gmail.com>
2024-02-25 22:39:39 +0300

    a

---
+++ Any_tasks/Multitheard/les1.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <thread>
+
+void foo()
+{
+    for (int i = 0; i < 10; i++)
+    {
+        std::cout << "foo\n";
+    }
+}
+
+int main()
+{
+    std::thread t(foo);
+    t.detach(); // говорит потоку, что он может работать независимо, но когда основной проток завершится то и этот тоже
+    for (int i = 0; i < 10; i++)
+    {
+        std::cout << "main\n";
+    }
+    //t.join() // теперь поток t будет завершон, основной поток будет ждать поток t
+
+
+    return 0;
+}
