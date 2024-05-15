3fac2deefe23a24456785247529485b2e89abca6
Egor213 <112801343+Egor213@users.noreply.github.com>
2024-05-16 01:30:49 +0300

    Update colloc.java

--- colloc.java
+++ colloc.java
@@ -5,97 +5,217 @@ import com.mathsystem.domain.plugin.plugintype.GraphProperty;
 
 import com.mathsystem.api.graph.model.Edge;
 
-import java.util.Set;
-import java.util.UUID;
+import java.util.*;
+
 import com.mathsystem.api.graph.model.Vertex;
-import java.util.Map;
-import java.util.HashSet;
-import java.util.Iterator;
+
 public class CheckDivSet implements GraphProperty {
 
-    Set<UUID> visited = new HashSet<>();
-
-    public void dfs(UUID node, Graph graph)
+    public class ColorNode
     {
-        if (!visited.contains(node)) {
-            visited.add(node);
-            for (Edge per : graph.getEdges())
-            {
-                if (graph.getDirectType() == GraphType.DIRECTED)
-                {
-                    if (per.getFromV().equals(node))
-                    {
-                        dfs(per.getToV(), graph);
-                    }
-                }
-                else
-                {
-                    if (per.getFromV().equals(node))
-                    {
-                        dfs(per.getToV(), graph);
-                    }
-                    else if(per.getToV().equals(node))
-                    {
-                        dfs(per.getFromV(), graph);
-                    }
-                }
+        public int color = 0;
+        public Vertex node;
+        public ColorNode(Vertex node) {
+            this.node = node;
+        }
+    }
 
+    public ColorNode getColorNode(UUID edge, Vector<ColorNode> set_color_node)
+    {
+        for (ColorNode temp_color : set_color_node) {
+            if (temp_color.node.getId().equals(edge)) {
+                return temp_color;
             }
         }
+        return null;
+    }
+
+    public void workNode(Graph graph, ColorNode color_node, Vector<ColorNode> set_color_node, boolean with_red_node, int number_color){
+        for (Edge edge : graph.getEdges()) {
+            if (edge.getFromV().equals(color_node.node.getId())) {
+                ColorNode temp_color = getColorNode(edge.getToV(), set_color_node);
+                dfsUndirection(temp_color, graph, number_color, set_color_node, with_red_node);
+            }
+            else if (edge.getToV().equals(color_node.node.getId())) {
+                ColorNode temp_color = getColorNode(edge.getFromV(), set_color_node);
+                dfsUndirection(temp_color, graph, number_color, set_color_node, with_red_node);
+            }
+        }
+    }
+
+    HashSet<Integer> unic_set = new HashSet<>();
+    HashSet<ColorNode> visited = new HashSet<>();
+    Vector<ColorNode> set_color_nodes = new Vector<>();
+    public void dfsUndirection(ColorNode color_node, Graph graph, int number_color, Vector<ColorNode> set_color_node, boolean with_red_node)
+    {
+        if (with_red_node) {
+            if (color_node.color == 0 && color_node.node.getColor() != Color.red) {
+                color_node.color = number_color;
+                workNode(graph, color_node, set_color_node, with_red_node, number_color);
+            }
+        }
+        else {
+            if (!visited.contains(color_node)) {
+                visited.add(color_node);
+                unic_set.add(color_node.color);
+                workNode(graph, color_node, set_color_node, with_red_node, number_color);
+            }
+        }
+    }
+
+    public boolean solveUndirection(Graph graph, boolean with_red_node)
+    {
+        int number_color = 0;
+        if (with_red_node) {
+            for (Vertex ver : graph.getVertices().values()) {
+                ColorNode temp = new ColorNode(ver);
+                set_color_nodes.add(temp);
+            }
+        }
+        for (ColorNode node : set_color_nodes) {
+            if (with_red_node) {
+                if (node.color == 0 && node.node.getColor() != Color.red) {
+                    number_color = number_color + 1;
+                    dfsUndirection(node, graph, number_color, set_color_nodes, with_red_node);
+                }
+            }
+            else {
+                if (!visited.contains(node)) {
+                    dfsUndirection(node, graph, number_color, set_color_nodes, with_red_node);
+                    if (unic_set.size() > 2){
+                        return true;
+                    }
+                    unic_set.clear();
+                }
+            }
+        }
+        return false;
+    }
+
+
+    //ДАЛЬШЕ ДЛЯ ОРИЕНТИРОВАННОГО ГРАФА
+    public Edge getCopyEdgeReverse(Edge edge)
+    {
+        Edge reverse_edge = new Edge();
+        reverse_edge.setToV(edge.getFromV());
+        reverse_edge.setFromV(edge.getToV());
+        reverse_edge.setColor(edge.getColor());
+        reverse_edge.setWeight(edge.getWeight());
+        reverse_edge.setLabel(edge.getLabel());
+        return reverse_edge;
+    }
+
+    public List<Edge> getReverseEdges(Graph graph){
+        List<Edge> temp = new ArrayList<>();
+        for (Edge edge : graph.getEdges()) {
+            Edge reverse_edge =getCopyEdgeReverse(edge);
+            temp.add(reverse_edge);
+        }
+        return temp;
+    }
+
+    Vector<ColorNode> save_time_quit = new Vector<>();
+    public void dfsSetTimeQuit(ColorNode color_node, List<Edge> reverse_edges, Vector<ColorNode> set_color_node, boolean with_red) {
+        if (!visited.contains(color_node)) {
+            if (!with_red) {
+                if (color_node.node.getColor() == Color.red) {
+                    return;
+                }
+            }
+            visited.add(color_node);
+            for (Edge edge : reverse_edges) {
+                if (edge.getFromV().equals(color_node.node.getId())) {
+                    ColorNode temp_color = getColorNode(edge.getToV(), set_color_node);
+                    dfsSetTimeQuit(temp_color, reverse_edges, set_color_node, with_red);
+                }
+            }
+            save_time_quit.add(color_node);
+        }
+    }
+
+    public void componentDfsNoRed(Graph graph, Vector<ColorNode> set_color_node, ColorNode color_node, int color_number) {
+        if (color_node.color == 0 && color_node.node.getColor() != Color.red) {
+            color_node.color = color_number;
+            for (Edge edge : graph.getEdges()) {
+                if (edge.getFromV().equals(color_node.node.getId())) {
+                    ColorNode temp_color = getColorNode(edge.getToV(), set_color_node);
+                    componentDfsNoRed(graph, set_color_nodes, temp_color, color_number);
+                }
+            }
+        }
+    }
+
+    public void setColorNodes(Graph graph) {
+        for (Vertex ver : graph.getVertices().values()) {
+            ColorNode temp = new ColorNode(ver);
+            set_color_nodes.add(temp);
+        }
+    }
+
+    public void checkDfsDirectional(Graph graph, Vector<ColorNode> set_color_node, ColorNode color_node) {
+        if (!visited.contains(color_node)) {
+            visited.add(color_node);
+            unic_set.add(color_node.color);
+            for (Edge edge : graph.getEdges()) {
+                if (edge.getFromV().equals(color_node.node.getId())) {
+                    ColorNode temp_color = getColorNode(edge.getToV(), set_color_node);
+                    checkDfsDirectional(graph, set_color_node, temp_color);
+                }
+            }
+        }
+    }
+
+    public boolean solve_direction(Graph graph, List<Edge> reverse_edges) {
+        setColorNodes(graph);
+        for (ColorNode color_node : set_color_nodes) {
+            dfsSetTimeQuit(color_node, reverse_edges, set_color_nodes, false);
+        }
+
+        int color_number = 0;
+        for (int i = save_time_quit.size() - 1; i >= 0; i--) {
+            ColorNode node = save_time_quit.get(i);
+            if (node.color == 0) {
+                color_number++;
+                componentDfsNoRed(graph, set_color_nodes, node, color_number);
+            }
+        }
+        for (ColorNode node : set_color_nodes) {
+            System.out.println(node.node.getLabel() + " Color: " + node.color);
+        }
+        visited.clear();
+        save_time_quit.clear();
+        for (ColorNode color_node : set_color_nodes) {
+            dfsSetTimeQuit(color_node, reverse_edges, set_color_nodes, true);
+        }
+        visited.clear();
+        for (int i = save_time_quit.size() - 1; i >= 0; i--) {
+            ColorNode node = save_time_quit.get(i);
+            checkDfsDirectional(graph, set_color_nodes, node);
+            if (unic_set.size() > 2){
+                return true;
+            }
+            unic_set.clear();
+        }
+
+        for (ColorNode node : set_color_nodes) {
+            System.out.println(node.node.getLabel() + " Color: " + node.color);
+        }
+
+        return false;
+
     }
 
 
     @Override
     public boolean execute(Graph graph) {
-        HashSet<UUID> delVertices = new HashSet<>();
-
-        Iterator<Map.Entry<UUID, Vertex>> iterator = graph.getVertices().entrySet().iterator();
-        while (iterator.hasNext()) {
-            Map.Entry<UUID, Vertex> entry = iterator.next();
-            if (entry.getValue().getColor() == Color.red) {
-                delVertices.add(entry.getKey());
-                iterator.remove();
-            }
+        if (graph.getDirectType() == GraphType.UNDIRECTED) {
+            solveUndirection(graph, true);
+            return solveUndirection(graph, false);
         }
-
-        if (delVertices.isEmpty())
-        {
-            return false;
+        else {
+            List<Edge> reverse_edges = getReverseEdges(graph);
+            return solve_direction(graph, reverse_edges);
         }
-
-        Iterator<Edge> iterator2 = graph.getEdges().iterator();
-        while (iterator2.hasNext()) {
-            Edge edge = iterator2.next();
-            if (delVertices.contains(edge.getToV()) || delVertices.contains(edge.getFromV())) {
-                iterator2.remove();
-            }
-        }
-
-
-
-        for (UUID id : graph.getVertices().keySet())
-        {
-            visited.clear();
-            dfs(id, graph);
-            Set<UUID> copySet = new HashSet<>(visited);
-            if (visited.size() != graph.getVertices().size())
-            {
-                for (UUID id2 : graph.getVertices().keySet())
-                {
-                    if (!copySet.contains(id2))
-                    {
-                        visited.clear();
-                        dfs(id2, graph);
-                        if (!visited.contains(id))
-                        {
-                            return true;
-                        }
-                    }
-
-                }
-            }
-        }
-        return false;
     }
 
 
